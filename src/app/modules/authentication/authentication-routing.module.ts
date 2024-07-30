51706bdd0817c2ad2e674ab8c0cf60cfed93acf1
Asim <asim@4iisolutions.com>
2024-07-29 23:59:02 -0700

    angular project setup with authentication module

---
+++ src/app/modules/authentication/authentication-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreatePasswordComponent } from './create-password/create-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { RegisterComponent } from './register/register.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: "login",
+    component: SignInComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "create/password",
+    component: CreatePasswordComponent
+  },
+  {
+    path: "forgot/password",
+    component: ForgotPasswordComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthenticationRoutingModule { }

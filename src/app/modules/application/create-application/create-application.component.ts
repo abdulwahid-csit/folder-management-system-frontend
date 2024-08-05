fc70d604ef393d057e615f7803cc0f4188599d8b
wahid <wahid@4iisolutions.com>
2024-08-05 13:30:57 +0500

    Organizaton and aplication module design changes.

--- src/app/modules/application/create-application/create-application.component.ts
+++ src/app/modules/application/create-application/create-application.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-create-application',
   templateUrl: './create-application.component.html',
   styleUrls: ['./create-application.component.scss']
 })
-export class CreateApplicationComponent {
+export class CreateApplicationComponent implements OnInit {
+  modalRef: any;
+  applicationForm!: FormGroup
+  modalOpen: boolean = false;
+
+
+  constructor(private modalService: BsModalService) { }
+
+
+  ngOnInit(): void {
+    this.applicationForm = new FormGroup({
+      applicationName: new FormControl('', [Validators.required]),
+      appUrl: new FormControl(''),
+      redirectUrl: new FormControl(''),
+      url: new FormControl(''),
+    })
+  }
+
+
+  closeModal(): void {
+    this.modalService.hide();
+  }
+
+  isControlHasError(controlName: any, validationType: string): boolean {
+    const control = this.applicationForm.controls[controlName];
+    if (!control) {
+      return false;
+    }
+    return (
+      control.hasError(validationType) && (control.dirty || control.touched)
+    );
+  }
+
+  submitForm() {
+    if (this.applicationForm.valid) {
+      console.log('Form Submitted');
+    }
+  }
+
 
 }

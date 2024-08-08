18dc81d1c308f1507155ebaa13eeb762d0605c80
Abu Bakar <abubakar@4iisolutions.com>
2024-08-08 12:10:22 +0500

    SSO-32, SSO, 31, SSO,30, SSO-29, SSO-28

--- src/app/modules/authentication/register/register.component.ts
+++ src/app/modules/authentication/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  constructor(private fb: FormBuilder,) {
 
+  }
+
+  registerForm!: FormGroup
+  ngOnInit(): void {
+
+    this.registerForm = this.fb.group({
+      name: [null, Validators.compose([Validators.required])],
+      password: [null, Validators.compose([Validators.required])],
+      email: [null, Validators.compose([Validators.required])],
+
+    })
+  }
+  isControlHasError(controlName: string, validationType: string): boolean {
+    const control = this.registerForm.controls[controlName];
+    if (!control) {
+      return false;
+    }
+    return (
+      control.hasError(validationType) && (control.dirty || control.touched)
+    );
+  }
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+
+    }
+    this.registerForm.markAllAsTouched();
+    if (this.registerForm.invalid) {
+
+      return;
+    }
+    console.log(this.registerForm.value);
+    ;
+  }
 }

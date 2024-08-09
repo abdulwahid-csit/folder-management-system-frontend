d26bd41514b6dd2294d32cde15e69d95be3508dc
wahid <wahid@4iisolutions.com>
2024-08-09 18:47:48 +0500

    SAV-6305, SAV-6306, SAV-6379

--- src/app/modules/organization/components/organization-details/organization-details.component.ts
+++ src/app/modules/organization/components/organization-details/organization-details.component.ts
@@ -166,6 +166,197 @@ export class OrganizationDetailsComponent implements OnInit {
     },
   ];
 
+  usersColumns: any = [];
+  usersList: any = []
+  userDataTable: any = [
+    {
+      data: {
+        columns: [
+          {
+            name: 'user',
+          },
+          {
+            name: 'Full Name',
+          },
+          {
+            name: 'Role',
+          },
+          {
+            name: 'Status',
+          },
+          {
+            name: 'Last Sign-in',
+          },
+        ],
+        payload: [
+
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Wahid",
+            "fullName": "Abdul Basit",
+            "role": "Admin",
+            "status": 'active',
+            "creationDate": 'May 24, 2023',
+          },
+        ]
+      }
+    }
+  ];
+
+  applicationColumn: any = [];
+  applicationList: any = [];
+  applicationDataTable: any = [
+    {
+      data: {
+        columns: [
+          {
+            name: 'Name',
+          },
+          {
+            name: 'App Id',
+          },
+          {
+            name: 'Url',
+          },
+          {
+            name: 'Creation Date',
+          },
+
+        ],
+        payload: [
+
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+          {
+            "id": 3,
+            "name": "Digital solutions",
+            "appId": "734832942349234",
+            "appUrl": "4iisolutions.com",
+            "creationDate": 'May 24, 2023',
+          },
+
+        ]
+      }
+    }
+  ]
   // assign the data from api like total_pages, payload_size
   tableConfig = {
     paginationParams: {
@@ -180,11 +371,18 @@ export class OrganizationDetailsComponent implements OnInit {
 
 
 
-  constructor(private modalService: BsModalService, private cdr:ChangeDetectorRef) { }
+  constructor(private modalService: BsModalService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
     this.columns = this.dataTable[0]?.data?.columns;
     this.organizationList = this.dataTable[0].data.payload;
+
+    this.usersColumns = this.userDataTable[0]?.data?.columns;
+    this.usersList = this.userDataTable[0].data.payload;
+
+    this.applicationColumn = this.applicationDataTable[0]?.data?.columns;
+    this.applicationList = this.applicationDataTable[0].data.payload;
+
 
     this.organizationForm = new FormGroup({
       organizationName: new FormControl('', [Validators.required]),
@@ -200,7 +398,7 @@ export class OrganizationDetailsComponent implements OnInit {
       domain: new FormControl('', [Validators.required]),
       status: new FormControl('', [Validators.required])
     })
-  
+
   }
 
   setSelectedTab(tab: string) {
@@ -250,9 +448,9 @@ export class OrganizationDetailsComponent implements OnInit {
   }
 
 
-  onEditOrganization(){
-    
-    if(this.organizationForm.invalid){
+  onEditOrganization() {
+
+    if (this.organizationForm.invalid) {
       this.editOrganizationForm.markAllAsTouched()
       this.cdr.detectChanges()
       return;

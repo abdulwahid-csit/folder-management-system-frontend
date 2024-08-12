e6d4e6536bf8fd96f43b02b37f1bd581a36231ca
Asim <asim@4iisolutions.com>
2024-08-12 06:51:20 -0700

    roles based functionality implemented

---
+++ src/app/guards/role.guard.ts
@@ -0,0 +1,18 @@
+import { CanActivateChildFn, Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+
+export const roleGuardChild: CanActivateChildFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const expectedRoles = route.parent?.data['expectedRoles'] as Array<string> || [];
+  const userRole = authService.getUserRole();
+
+  if (expectedRoles.includes(userRole) || userRole === 'Master') {
+    return true;
+  }
+
+  router.navigate(['/unauthorized']);
+  return false;
+};

6a32398718cbee72b216fb1b0705bdc43eaff8bf
Asim <asim@4iisolutions.com>
2024-08-12 00:14:38 -0700

    crud functionality

--- src/app/interceptors/auth.ts
+++ src/app/interceptors/auth.ts
@@ -5,21 +5,21 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  // Intercept HTTP requests
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // Clone the request and add new headers
+
     const authReq = req.clone({
       setHeaders: {
-        'Content-Type': 'application/json', // Example header
-        'Authorization': `Bearer ${this.getToken()}` // Example header with token
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.getToken()}`
       }
     });
 
-    // Pass the cloned request to the next handler
+
     return next.handle(authReq);
   }
 
-  // Retrieve the token from local storage or any other secure place
+
   private getToken(): string | null {
     return localStorage.getItem('access_token');
   }

a49fbc2d480b04173a22bb9cfd19c5316a25d626
Abu Bakar <abubakar@4iisolutions.com>
2024-08-14 10:59:58 +0500

    SSO-49, SSO-50, SSO-51, SSO-52

--- src/app/modules/authentication/create-password/create-password.component.ts
+++ src/app/modules/authentication/create-password/create-password.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,40 +6,44 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './create-password.component.html',
   styleUrls: ['./create-password.component.scss']
 })
-export class CreatePasswordComponent {
-  constructor(private fb: FormBuilder,) {
+export class CreatePasswordComponent implements OnInit {
+  passwordForm!: FormGroup;
+  hidePassword: boolean = true;
+  hideConfirmPassword: boolean = true;
 
-  }
+  constructor(private fb: FormBuilder) { }
 
-  passwordForm!: FormGroup
   ngOnInit(): void {
-
     this.passwordForm = this.fb.group({
-
-      password: [null, Validators.compose([Validators.required])],
-      confirmPassword: [null, Validators.compose([Validators.required])],
-
-    })
+      password: [null, [Validators.required, Validators.minLength(8)]],
+      confirmPassword: [null, Validators.required]
+    }, { validator: this.passwordMatchValidator });
   }
+
+  passwordMatchValidator(form: FormGroup): { [key: string]: boolean } | null {
+    const password = form.get('password')?.value;
+    const confirmPassword = form.get('confirmPassword')?.value;
+    return password && confirmPassword && password !== confirmPassword ? { 'mismatch': true } : null;
+  }
+
   isControlHasError(controlName: string, validationType: string): boolean {
-    const control = this.passwordForm.controls[controlName];
-    if (!control) {
-      return false;
-    }
-    return (
-      control.hasError(validationType) && (control.dirty || control.touched)
-    );
+    const control = this.passwordForm.get(controlName);
+    return control ? control.hasError(validationType) && (control.dirty || control.touched) : false;
   }
+
   onSubmit(): void {
-    if (this.passwordForm.valid) {
-
-    }
-    this.passwordForm.markAllAsTouched();
     if (this.passwordForm.invalid) {
-
+      this.passwordForm.markAllAsTouched();
       return;
     }
     console.log(this.passwordForm.value);
-    ;
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  toggleConfirmPasswordVisibility(): void {
+    this.hideConfirmPassword = !this.hideConfirmPassword;
   }
 }

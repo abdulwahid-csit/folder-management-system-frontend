78c7171478739be17ecb4ec42f747f4e0a2953e1
Abu Bakar <abubakar@4iisolutions.com>
2024-08-22 11:25:07 +0500

    SSO-67, SSO-68

--- src/app/modules/authentication/register/register.component.ts
+++ src/app/modules/authentication/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -10,12 +13,19 @@ export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
   hidePassword = true;
   isPasswordVisible: boolean = false;
-  constructor(private fb: FormBuilder) { }
+  isLoading: boolean = false;
+  
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private toast: ToastrService
+  ) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
-      name: [null, Validators.required],
-      email: [null, [Validators.required, Validators.email]],  
+      userName: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]],
       password: [null, Validators.compose([Validators.required])],
     });
   }
@@ -29,12 +39,27 @@ export class RegisterComponent implements OnInit {
   }
    
   onSubmit(): void {
-    if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
-      // Handle form submission here
-    } else {
+    if (this.registerForm.invalid) {
       this.registerForm.markAllAsTouched();
+      return;
     }
+    const { email, password, userName } = this.registerForm.value;
+    this.isLoading = true;
+    this.authService.signUp(email, password, userName).subscribe((response: any) => {
+      if (response.status_code === 201) {
+        const returnUrl = this.route.snapshot.queryParamMap.get('returnUrl') || '/layout';
+        this.router.navigateByUrl(returnUrl);
+        const { access_token, refresh_token, access_token_expires, user } = response.data;
+        this.authService.storeTokens(access_token, refresh_token, access_token_expires, user);
+        this.router.navigate(['/layout']);
+      } else {
+        this.toast.error(response.message, "Error!");
+      }
+      this.isLoading = false;
+    }, error => {
+      this.toast.error(error.error.message, "Error!");
+      this.isLoading = false;
+    });
   }
 
   onControlBlur(controlName: string): void {
@@ -43,6 +68,7 @@ export class RegisterComponent implements OnInit {
       control.markAsTouched();
     }
   }
+
   togglePasswordVisibility(): void {
     this.hidePassword = !this.hidePassword;
   }
